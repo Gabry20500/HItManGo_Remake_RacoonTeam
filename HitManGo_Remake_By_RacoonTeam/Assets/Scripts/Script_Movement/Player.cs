0559acd8111a1787de85c25c0774884c4f68a95a
Gabriele Fiorentino <gabrielefiorentino2@gmail.com>
2022-11-25 14:58:51 +0100

    Add animation

--- HitManGo_Remake_By_RacoonTeam/Assets/Scripts/Script_Movement/Player.cs
+++ HitManGo_Remake_By_RacoonTeam/Assets/Scripts/Script_Movement/Player.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class Player : MonoBehaviour
@@ -7,15 +8,65 @@ public class Player : MonoBehaviour
 
     public void Move(Vector2 direction)
     {
-        foreach (Node node in currentNode.linkedNodes)
+        if (canMove)
         {
-            Vector3 nodeDir = node.position - transform.position;
-            Vector2 nodeDir2D = new Vector2(nodeDir.x, nodeDir.z);
-            if (Vector2.Dot(nodeDir2D, direction) > 0.9f)
+            canMove = false;
+            foreach (Node node in currentNode.linkedNodes)
             {
-                currentNode = node;
-                transform.position = currentNode.position;
+                Vector3 nodeDir = node.position - transform.position;
+                Vector2 nodeDir2D = new Vector2(nodeDir.x, nodeDir.z);
+                if (Vector2.Dot(nodeDir2D, direction) > 0.9f)
+                {
+                    currentNode = node;
+                    StartCoroutine(Animation(currentNode.position));
+                }
             }
+        }
+    }
+
+    private IEnumerator Animation(Vector3 destination)
+    {
+        Vector3 startPos = transform.position;
+        Vector3 endPos = new Vector3(destination.x, transform.position.y, destination.z);
+
+        Vector3 dir = (endPos + startPos) / 2;
+        Vector3 midPos = new Vector3(dir.x, (dir.y + 10f), dir.z);
+
+        yield return StartCoroutine(goMidPos(startPos, midPos));
+        yield return StartCoroutine(goEndPos(midPos, endPos));
+
+        transform.position = endPos;
+        canMove = true;
+        yield return null;
+    }
+
+    private IEnumerator goMidPos(Vector3 startPos, Vector3 midPos)
+    {
+        float elapsed = 0f;
+        float duration = 0.5f;
+
+        while (elapsed < duration)
+        {
+            float perc = elapsed / duration;
+            
+            transform.position = Vector3.Lerp(startPos, midPos, perc);
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+    }
+    
+    private IEnumerator goEndPos(Vector3 midPos, Vector3 endPos)
+    {
+        float elapsed = 0f;
+        float duration = 0.5f;
+
+        while (elapsed < duration)
+        {
+            float perc = elapsed / duration;
+
+            transform.position = Vector3.Lerp(midPos, endPos, perc);
+            elapsed += Time.deltaTime;
+            yield return null;
         }
     }
 }
