2de98a909eda6ab80a296f3516e6943c67b90e5e
FilippoRoveda <98386364+FilippoRoveda@users.noreply.github.com>
2022-12-22 11:40:38 +0100

    MainMenù implementation

--- HitManGo_Remake_By_RacoonTeam/Assets/Scripts/Script_Menu/MainMenu.cs
+++ HitManGo_Remake_By_RacoonTeam/Assets/Scripts/Script_Menu/MainMenu.cs
@@ -1,10 +1,17 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] List<GameObject> objectsToDisable;
+    [SerializeField] List<GameObject> objectToEnable;
+    private AudioPlayer musicPlayer;
+
+    private void Awake()
+    {
+        musicPlayer = FindObjectOfType<AudioPlayer>();
+    }
     public void GoToScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
@@ -15,5 +22,46 @@ public class MainMenu : MonoBehaviour
     {
         Application.Quit();
         Debug.Log("Game has quit.");
+    }
+
+    public void OpenOptions() 
+    { 
+        foreach(GameObject go in objectsToDisable)
+        {
+            go.SetActive(false);
+        }
+        foreach (GameObject go in objectToEnable)
+        {
+            go.SetActive(true);
+        }
+    }
+
+    public void CloseOptions()
+    {
+        foreach (GameObject go in objectsToDisable)
+        {
+            go.SetActive(true);
+        }
+        foreach (GameObject go in objectToEnable)
+        {
+            go.SetActive(false);
+        }
+    }
+
+    public void ShowCredits()
+    {
+
+    }
+
+    public void MusicOnOff()
+    {
+        if(musicPlayer.GetComponent<AudioSource>().volume != 0.0f)
+        {
+            musicPlayer.GetComponent<AudioSource>().volume = 0.0f;
+        }
+        else
+        {
+            musicPlayer.GetComponent<AudioSource>().volume = 1.0f;
+        }
     }
 }
